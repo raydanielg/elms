cd8e52c95eebe15a011233774d6af5741eb1dd24
raydanielg <airezra2+1@gmail.com>
2026-07-16 12:00:37 +0300

    commit

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class Notification extends Model
 {
@@ -14,6 +15,15 @@ class Notification extends Model
     public $incrementing = false;
 
     protected $fillable = ['id', 'user_id', 'type', 'title', 'message', 'data', 'read_at'];
+
+    protected static function booted(): void
+    {
+        static::creating(function (Notification $notification) {
+            if (empty($notification->id)) {
+                $notification->id = (string) Str::uuid();
+            }
+        });
+    }
 
     protected function casts(): array
     {
